512b2c8165cd588e16c6765d403c7fbdedf13884
Deniks <rezanovich,denis@gmail.com>
2021-07-29 17:43:04 +0100

    changed hard-coded css to style-components

--- src/Components/ProjectCard/Image.js
+++ src/Components/ProjectCard/Image.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useSpring, animated, config } from 'react-spring';
 import { Image as GrommetImage } from 'grommet';
+import styled from 'styled-components';
 
-const AnimatedImage = animated(GrommetImage);
+const StyledImage = styled(GrommetImage)`
+  max-width: 100% !important;
+`;
+const AnimatedImage = animated(StyledImage);
 const Image = ({ src, zoom }) => {
   const imageStyles = useSpring({
     transform: zoom ? 'scale(1.2)' : 'scale(1.05)',
